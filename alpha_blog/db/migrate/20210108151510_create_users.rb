eddc2fce9f5014f222084c40930b53e8c8e03ba0
srijan <srijansingh53@gmail.com>
2021-01-09 00:12:03 +0530

    user validations

--- alpha_blog/db/migrate/20210108151510_create_users.rb
+++ alpha_blog/db/migrate/20210108151510_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :username
-      t.stringring :email
+      t.string :email
       t.timestamps
     end
   end
