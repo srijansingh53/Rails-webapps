fd1a7c5b7493a1051ee9609df7d380da7acf7fc0
srijan <srijansingh53@gmail.com>
2021-01-09 20:08:14 +0530

    password not catching

---
+++ alpha_blog/app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    byebug
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog. You have successfully signed in."
+      redirect_to_articles_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require[:user].permit[:username, :email, :password]
+  end
+end
